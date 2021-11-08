d4dc4a8e18bea86ccbda115681b3011c0d93142f
szaqual44 <szaqual44@gmail.com>
2021-11-08 10:01:47 +0100

    firebase auth implemented

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './css/main.css';
+import './styles/main.css'
 import App from './App';
 import MyTheme from './styles/myTheme'
-// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <React.StrictMode>
